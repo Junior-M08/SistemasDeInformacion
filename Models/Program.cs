815eb36767661e1dccc0e9f5e328e533b485571e
Junior Mayorga <mayorgajunior423@gmail.com>
2023-07-17 12:59:10 -0600

    Clases agregadas a modelos

--- Models/Program.cs
+++ Models/Program.cs
@@ -1,7 +1,13 @@
+using Microsoft.EntityFrameworkCore;
+using SistemasDeInformacioinMVC;
+
 var builder = WebApplication.CreateBuilder(args);
 
 // Add services to the container.
 builder.Services.AddControllersWithViews();
+
+builder.Services.AddDbContext<ProyectoContext>(options =>
+        options.UseSqlServer(builder.Configuration.GetConnectionString("conexion")));
 
 var app = builder.Build();
 
---
+++ Models/ProyectoContext.cs
@@ -0,0 +1,230 @@
+﻿using System;
+using System.Collections.Generic;
+using Microsoft.EntityFrameworkCore;
+using Microsoft.EntityFrameworkCore.Metadata;
+
+namespace SistemasDeInformacioinMVC
+{
+    public partial class ProyectoContext : DbContext
+    {
+        public ProyectoContext()
+        {
+        }
+
+        public ProyectoContext(DbContextOptions<ProyectoContext> options)
+            : base(options)
+        {
+        }
+
+        public virtual DbSet<Categoria> Categoria { get; set; } = null!;
+        public virtual DbSet<DetalleVenta> DetalleVenta { get; set; } = null!;
+        public virtual DbSet<NumeroDocumento> NumeroDocumentos { get; set; } = null!;
+        public virtual DbSet<Producto> Productos { get; set; } = null!;
+        public virtual DbSet<Rol> Rols { get; set; } = null!;
+        public virtual DbSet<Usuario> Usuarios { get; set; } = null!;
+        public virtual DbSet<Venta> Venta { get; set; } = null!;
+
+        protected override void OnConfiguring(DbContextOptionsBuilder optionsBuilder)
+        {
+            if (!optionsBuilder.IsConfigured)
+            {
+//#warning To protect potentially sensitive information in your connection string, you should move it out of source code. You can avoid scaffolding the connection string by using the Name= syntax to read it from configuration - see https://go.microsoft.com/fwlink/?linkid=2131148. For more guidance on storing connection strings, see http://go.microsoft.com/fwlink/?LinkId=723263.
+  //              optionsBuilder.UseSqlServer("Server=ADMINISTRATOR\\SQLEXPRESS; DataBase=Proyecto;Integrated Security=true; Encrypt=False");
+            }
+        }
+
+        protected override void OnModelCreating(ModelBuilder modelBuilder)
+        {
+            modelBuilder.Entity<Categoria>(entity =>
+            {
+                entity.HasKey(e => e.IdCategoria)
+                    .HasName("PK__Categori__8A3D240CB5EBF5C5");
+
+                entity.Property(e => e.IdCategoria).HasColumnName("idCategoria");
+
+                entity.Property(e => e.Descripcion)
+                    .HasMaxLength(50)
+                    .IsUnicode(false)
+                    .HasColumnName("descripcion");
+
+                entity.Property(e => e.EsActivo).HasColumnName("esActivo");
+
+                entity.Property(e => e.FechaRegistro)
+                    .HasColumnType("datetime")
+                    .HasColumnName("fechaRegistro")
+                    .HasDefaultValueSql("(getdate())");
+            });
+
+            modelBuilder.Entity<DetalleVenta>(entity =>
+            {
+                entity.HasKey(e => e.IdDetalleVenta)
+                    .HasName("PK__DetalleV__BFE2843FDB150EC1");
+
+                entity.Property(e => e.IdDetalleVenta).HasColumnName("idDetalleVenta");
+
+                entity.Property(e => e.Cantidad).HasColumnName("cantidad");
+
+                entity.Property(e => e.IdProducto).HasColumnName("idProducto");
+
+                entity.Property(e => e.IdVenta).HasColumnName("idVenta");
+
+                entity.Property(e => e.Precio)
+                    .HasColumnType("decimal(10, 2)")
+                    .HasColumnName("precio");
+
+                entity.Property(e => e.Total)
+                    .HasColumnType("decimal(10, 2)")
+                    .HasColumnName("total");
+
+                entity.HasOne(d => d.IdProductoNavigation)
+                    .WithMany(p => p.DetalleVenta)
+                    .HasForeignKey(d => d.IdProducto)
+                    .HasConstraintName("FK__DetalleVe__idPro__49C3F6B7");
+
+                entity.HasOne(d => d.IdVentaNavigation)
+                    .WithMany(p => p.DetalleVenta)
+                    .HasForeignKey(d => d.IdVenta)
+                    .HasConstraintName("FK__DetalleVe__idVen__48CFD27E");
+            });
+
+            modelBuilder.Entity<NumeroDocumento>(entity =>
+            {
+                entity.HasKey(e => e.IdNumeroDocumento)
+                    .HasName("PK__NumeroDo__471E421A7316DEC7");
+
+                entity.ToTable("NumeroDocumento");
+
+                entity.Property(e => e.IdNumeroDocumento).HasColumnName("idNumeroDocumento");
+
+                entity.Property(e => e.FechaRegistro)
+                    .HasColumnType("datetime")
+                    .HasColumnName("fechaRegistro")
+                    .HasDefaultValueSql("(getdate())");
+
+                entity.Property(e => e.UltimoNumero).HasColumnName("ultimo_Numero");
+            });
+
+            modelBuilder.Entity<Producto>(entity =>
+            {
+                entity.HasKey(e => e.IdProducto)
+                    .HasName("PK__Producto__07F4A13277B3610B");
+
+                entity.ToTable("Producto");
+
+                entity.Property(e => e.IdProducto).HasColumnName("idProducto");
+
+                entity.Property(e => e.EsActivo).HasColumnName("esActivo");
+
+                entity.Property(e => e.FechaRegistro)
+                    .HasColumnType("datetime")
+                    .HasColumnName("fechaRegistro")
+                    .HasDefaultValueSql("(getdate())");
+
+                entity.Property(e => e.IdCategoria).HasColumnName("idCategoria");
+
+                entity.Property(e => e.Nombre)
+                    .HasMaxLength(100)
+                    .IsUnicode(false)
+                    .HasColumnName("nombre");
+
+                entity.Property(e => e.Precio)
+                    .HasColumnType("decimal(10, 2)")
+                    .HasColumnName("precio");
+
+                entity.Property(e => e.Stock).HasColumnName("stock");
+
+                entity.HasOne(d => d.IdCategoriaNavigation)
+                    .WithMany(p => p.Productos)
+                    .HasForeignKey(d => d.IdCategoria)
+                    .HasConstraintName("FK__Producto__idCate__3F466844");
+            });
+
+            modelBuilder.Entity<Rol>(entity =>
+            {
+                entity.HasKey(e => e.IdRol)
+                    .HasName("PK__Rol__3C872F76D7BE58D5");
+
+                entity.ToTable("Rol");
+
+                entity.Property(e => e.IdRol).HasColumnName("idRol");
+
+                entity.Property(e => e.Descripcion)
+                    .HasMaxLength(50)
+                    .IsUnicode(false)
+                    .HasColumnName("descripcion");
+
+                entity.Property(e => e.EsActivo).HasColumnName("esActivo");
+
+                entity.Property(e => e.FechaRegistro)
+                    .HasColumnType("datetime")
+                    .HasColumnName("fechaRegistro")
+                    .HasDefaultValueSql("(getdate())");
+            });
+
+            modelBuilder.Entity<Usuario>(entity =>
+            {
+                entity.HasKey(e => e.IdUsuario)
+                    .HasName("PK__Usuario__645723A60BE043B6");
+
+                entity.ToTable("Usuario");
+
+                entity.Property(e => e.IdUsuario).HasColumnName("idUsuario");
+
+                entity.Property(e => e.Clave)
+                    .HasMaxLength(40)
+                    .IsUnicode(false)
+                    .HasColumnName("clave");
+
+                entity.Property(e => e.Correo)
+                    .HasMaxLength(40)
+                    .IsUnicode(false)
+                    .HasColumnName("correo");
+
+                entity.Property(e => e.EsActivo).HasColumnName("esActivo");
+
+                entity.Property(e => e.IdRol).HasColumnName("idRol");
+
+                entity.Property(e => e.NombreApellidos)
+                    .HasMaxLength(100)
+                    .IsUnicode(false)
+                    .HasColumnName("nombreApellidos");
+
+                entity.HasOne(d => d.IdRolNavigation)
+                    .WithMany(p => p.Usuarios)
+                    .HasForeignKey(d => d.IdRol)
+                    .HasConstraintName("FK__Usuario__idRol__398D8EEE");
+            });
+
+            modelBuilder.Entity<Venta>(entity =>
+            {
+                entity.HasKey(e => e.IdVenta)
+                    .HasName("PK__Venta__077D56148B2EFD78");
+
+                entity.Property(e => e.IdVenta).HasColumnName("idVenta");
+
+                entity.Property(e => e.FechaRegistro)
+                    .HasColumnType("datetime")
+                    .HasColumnName("fechaRegistro")
+                    .HasDefaultValueSql("(getdate())");
+
+                entity.Property(e => e.NumeroDocumento)
+                    .HasMaxLength(40)
+                    .IsUnicode(false)
+                    .HasColumnName("numeroDocumento");
+
+                entity.Property(e => e.TipoPago)
+                    .HasMaxLength(50)
+                    .IsUnicode(false)
+                    .HasColumnName("tipoPago");
+
+                entity.Property(e => e.Total)
+                    .HasColumnType("decimal(10, 2)")
+                    .HasColumnName("total");
+            });
+
+            OnModelCreatingPartial(modelBuilder);
+        }
+
+        partial void OnModelCreatingPartial(ModelBuilder modelBuilder);
+    }
+}
